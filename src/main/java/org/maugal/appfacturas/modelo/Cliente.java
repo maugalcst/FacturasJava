6af7ebaffdff375727f6c340ead6fd790c783f81
maugalcst <maugal.cst@gmail.com>
2025-02-05 13:17:15 -0600

    add model package

---
+++ src/main/java/org/maugal/appfacturas/modelo/Cliente.java
@@ -0,0 +1,23 @@
+package org.maugal.appfacturas.modelo;
+
+public class Cliente {
+
+    private String nombre;
+    protected String nif;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNif() {
+        return nif;
+    }
+
+    public void setNif(String nif) {
+        this.nif = nif;
+    }
+}
